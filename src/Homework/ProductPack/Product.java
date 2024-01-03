1c58348cc942026c466ad3156fb011b75563a1a4
mohamad <agbamoh@gmail.com>
2024-01-03 18:21:48 +0200

    finished Product

---
+++ src/Homework/ProductPack/Product.java
@@ -0,0 +1,41 @@
+package Homework.ProductPack;
+
+public class Product {
+    private String name;
+    private double price;
+    private static final double VAT=0.17;
+
+    public Product() {
+        this.name="";
+        this.price=0.0;
+    }
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+    public String display() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
